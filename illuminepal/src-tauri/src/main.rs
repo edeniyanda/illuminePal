ae7e4e5ddd9340b4db6f60699a4ad908086729db
Eden Iyanda <106480499+edeniyanda@users.noreply.github.com>
2025-05-06 19:04:31 +0100

    feat: add load_settings and save_settings commands for managing application settings

--- illuminepal/src-tauri/src/main.rs
+++ illuminepal/src-tauri/src/main.rs
@@ -1,15 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://v1.tauri.app/v1/guides/features/command
+
+mod settings;
+
+use settings::{AppSettings}; // Add this if you reference types on the frontend
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn load_settings() -> AppSettings {
+    AppSettings::load()
+}
+
+#[tauri::command]
+fn save_settings(new_settings: AppSettings) {
+    new_settings.save();
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            load_settings,
+            save_settings
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
